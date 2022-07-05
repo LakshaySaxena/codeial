dfa594ff1925ec2bcad1c1f092d5d958bd82e939
Lakshay Saxena <lakshaysaxena8@gmail.com>
2022-07-05 20:18:12 +0530

    setting up name of the poster

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,14 +1,26 @@
+const Post = require('../models/post');
 module.exports.home= function(req,res){
 //return res.end('<h1>Express is up<h1>');
 
 // edited cookie value from chrome>>pplication and printed to see
-console.log(req.cookies);
+// console.log(req.cookies);
 
-//changing user1 cookie value to 33
-res.cookie('user1', 33);
-return res.render('home',{
-    title: "Codeial"
-});
+// //changing user1 cookie value to 33
+// res.cookie('user1', 33);
+// Post.find({}, function(err, posts){
+//         return res.render('home', {
+//             title: "Codeial | Home",
+//             posts:  posts
+//         });
+//     });
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
+
 }
 
 module.exports.welcome= function(req,res){
