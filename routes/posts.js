8ba203ad6e3fcb265d430071e0f8e58e20713f55
Lakshay Saxena <lakshaysaxena8@gmail.com>
2022-07-06 15:22:05 +0530

    check authentication while creating post

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const PostController = require('../controllers/posts_controller');
 
-router.post('/create',PostController.create);
+
+//passport.checkAuthentication--> if user is signed in only thrn create form should be visible
+router.post('/create',passport.checkAuthentication,PostController.create);
 
 
 module.exports = router;
